9d2ecf618f8cbc9904d36f2a42b34f090280fe8d
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-04-02 18:35:17 +0300

    backend preparation in place

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('server is working');
+import * as dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+dotenv.config();
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+
+// const getData = async () => {
+//   const res = await fetch(
+//     'https://www.course-api.com/react-useReducer-cart-project'
+//   );
+//   const cartData = await res.json();
+//   console.log(cartData);
+// };
+// getData();
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'Data received', data: req.body });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(5100, () => console.log(`server is running at port ${port}...`));
