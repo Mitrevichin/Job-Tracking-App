baa1d140797ba8058f2e2e7e8e1d144f81ffc856
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-04-11 20:12:37 +0300

    validation middleware added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import { body, validationResult } from 'express-validator';
+import { validateTest } from './middleware/validationMiddleware.js';
 
 import jobRouter from './routes/jobRouter.js';
 
@@ -21,28 +21,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post(
-  '/api/v1/test',
-  [
-    body('name')
-      .notEmpty()
-      .withMessage('name is required')
-      .isLength({ min: 50 })
-      .withMessage('name must be at least 50 chars'),
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map(error => error.msg);
-      return res.status(400).json({ errors: errorMessages });
-    }
-    next();
-  },
-  (req, res) => {
-    const { name } = req.body;
-    res.json({ msg: `hello ${name}` });
-  }
-);
+app.post('/api/v1/test', validateTest, (req, res) => {
+  const { name } = req.body;
+  res.json({ msg: `hello ${name}` });
+});
 
 app.use('/api/v1/jobs', jobRouter);
 
