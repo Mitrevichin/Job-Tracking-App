70470ab04768ff27c9a69f1e71364caebb706de2
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-04-26 15:10:59 +0300

    set tup success and error toasts

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,13 @@
+// useNavigation is only for use inside a React Router app. It depends on React Router's internal navigation state.
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import customFetch from '../utils/customFetch';
 import { Logo, FormRow } from '../components';
+import { toast } from 'react-toastify';
 
+// React Router action is a way to handle form submissions and server communication directly inside the routing system.
+// An action is a function you define for a route.
+// It runs when a form is submitted with <Form method="post" /> (or other HTTP methods like PUT, DELETE).
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
@@ -11,14 +16,20 @@ export const action = async ({ request }) => {
     // We don't need a variable cause in the front-end we won't use the registered user and the server is not sending it. It sends just a message
     await customFetch.post('/auth/register', data);
     // redirect is only used within react-router actions
+    toast.success('Registration successful.');
     return redirect('/login');
   } catch (error) {
     console.log(error);
+    toast.error(error?.response?.data?.message);
     return error;
   }
 };
 
 function Register() {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
       <Form method='post' className='form'>
@@ -34,7 +45,9 @@ function Register() {
         <FormRow type='text' name='location' defaultValue='earth' />
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
-        <button className='btn btn-block'>Submit</button>
+        <button className='btn btn-block' disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting' : 'Submit'}
+        </button>
         <p>
           Already a member?
           <Link to='/login' className='member-btn'>
