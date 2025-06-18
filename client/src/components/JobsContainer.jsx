722636724cd214166414b969ea4c19dd470d199f
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-06-18 20:44:46 +0300

    all jobs structure set

---
+++ client/src/components/JobsContainer.jsx
@@ -0,0 +1,5 @@
+function JobsContainer() {
+  return <h2>JobsContainer</h2>;
+}
+
+export default JobsContainer;
---
+++ client/src/components/SearchContainer.jsx
@@ -0,0 +1,5 @@
+function SearchContainer() {
+  return <h2>SearchContainer</h2>;
+}
+
+export default SearchContainer;
