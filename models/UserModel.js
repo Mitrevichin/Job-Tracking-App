39bb66a41baa1496d92b09e47e32b9b57f4d0bbe
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-06-25 17:56:40 +0300

    profile page structure created

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { type } from 'os';
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -16,6 +17,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     enum: ['user', 'admin'],
     default: 'user',
+  },
+  avatar: {
+    type: String,
+  },
+  avatarPublicId: {
+    type: String,
   },
 });
 
