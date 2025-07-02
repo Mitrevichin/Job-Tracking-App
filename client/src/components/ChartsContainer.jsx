ab2e428db7eb5593f1d515ea6f639f6f698fd023
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-07-02 13:29:15 +0300

    Stats container and Bar Charts container added

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,5 +1,20 @@
-function ChartsContainer() {
-  return <div>ChartsContainer</div>;
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import { useState } from 'react';
+
+function ChartsContainer({ data }) {
+  const [barChart, setBarChar] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button onClick={() => setBarChar(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 }
 
 export default ChartsContainer;
