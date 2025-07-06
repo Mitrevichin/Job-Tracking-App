9893a0470e6d33aed5e00ff44887b7fed1c00961
Petar Mitrevichin <pmitrevichin@gmail.com>
2025-07-06 20:11:47 +0300

    deployment set up

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
 // The order matters. If the request is not one of the CRUD methods above
 // it's gonna enter this middleware
 app.use('*', (req, res) => {
